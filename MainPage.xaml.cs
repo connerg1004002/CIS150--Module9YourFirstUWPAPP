4acf490970992cfa57eeb9c581e4d0b260b64d33
GabrielConner <gabriel.conner2008@gmail.com>
2025-03-25 21:06:16 -0500

    project

---
+++ MainPage.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.Storage;
+
+
+namespace CIS150__Module9YourFirstUWPAPP {
+  public sealed partial class MainPage : Page {
+    public Student newStudent = new Student();
+    public ObservableCollection<Student> studentList = new ObservableCollection<Student>();
+
+    public MainPage() {
+      this.InitializeComponent();
+    }
+
+    public void SubmitButton_Click(object sender, RoutedEventArgs args) {
+      if (newStudent.AnyNull())
+        return;
+
+      studentList.Add(new Student(newStudent));
+      newStudent.EqualsNew();
+    }
+
+    public void CancelButton_Click(object sender, RoutedEventArgs args) {
+      if (newStudent.AnyNull())
+        return;
+
+      newStudent.EqualsNew();
+    }
+
+
+
+    //Pops up a file creation menu
+    public async void SaveButton_Click(object sender, RoutedEventArgs args) {
+      if (studentList.Count <= 0)
+        return;
+
+      var picker = new FileSavePicker();
+      picker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+      picker.FileTypeChoices.Add("Comma Seperated Values", new List<string>() { ".csv" });
+      picker.SuggestedFileName = "studentSaves";
+
+      StorageFile file = await picker.PickSaveFileAsync();
+      if (file is null)
+        return;
+
+      foreach (Student student in studentList) {
+        await FileIO.AppendTextAsync(file, student.AsCSV());
+      }
+
+      Windows.Storage.Provider.FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
+      if (status != Windows.Storage.Provider.FileUpdateStatus.Complete)
+        throw new IOException("Could not save file");
+    }
+
+
+    //Deletes all students currently in list
+    public void DeleteAllButton_Click(object sender, RoutedEventArgs args) {
+      studentList.Clear();
+    }
+
+
+    //Pops up a file selection menu
+    public async void OpenButton_Click(object sender, RoutedEventArgs args) {
+      var picker = new FileOpenPicker();
+      picker.ViewMode = PickerViewMode.List;
+      picker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+      picker.FileTypeFilter.Add(".csv");
+
+      StorageFile file = await picker.PickSingleFileAsync();
+      if (file is null || file.Path is null)
+        return;
+
+      studentList.Clear();
+
+      string wholeFile = await FileIO.ReadTextAsync(file);
+      string[] lines = wholeFile.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+      for (int i = 0; i < lines.Count(); i++) {
+        string line = lines[i];
+        if (line is null)
+          break;
+        if (!Student.TryParse(line, out Student student))
+          continue;
+        studentList.Add(student);
+      }
+    }
+    
+
+  }
+}
---
+++ Student.cs
@@ -0,0 +1,119 @@
+using System;
+using System.ComponentModel;
+using System.Linq;
+
+public class Student : INotifyPropertyChanged {
+  public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+  //Allows ID input field to be empty
+  public string idAsString {
+    get {
+      if (ID < 0)
+        return "";
+      return ID.ToString();
+    }
+    set {
+      if (string.IsNullOrEmpty(value) || !int.TryParse(value, out ID)) ID = -1;
+      PropertyChanged.Invoke(this, new PropertyChangedEventArgs("idAsString"));
+    }
+  }
+
+  //Allows CourseGrade input field to be empty
+  public string courseGradeAsString {
+    get {
+      if (CourseGrade < 0)
+        return "";
+      return CourseGrade.ToString();
+    }
+    set {
+      if (string.IsNullOrEmpty(value) || !float.TryParse(value, out CourseGrade)) CourseGrade = -1;
+      PropertyChanged.Invoke(this, new PropertyChangedEventArgs("courseGradeAsString"));
+    }
+  }
+
+
+
+
+  public int ID;
+  public string FirstName { get; set; }
+  public string LastName { get; set; }
+  public string Course { get; set; }
+  public float CourseGrade;
+  public string AsString { get; set; }
+
+
+
+  public void EqualsNew() {
+    ID = -1;
+    FirstName = "";
+    LastName = "";
+    Course = "";
+    CourseGrade = -1;
+    UpdateAll();
+  }
+
+  //Updates uwp and regenerates the 'AsString' property
+  public void UpdateAll() {
+    AsString = ToString();
+    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(""));
+  }
+
+  public bool AnyNull() {
+    return ID < 0 || FirstName == "" || LastName == "" || Course == "" || CourseGrade < 0;
+  }
+
+  public override string ToString() {
+    return $"{ID} - {LastName}, {FirstName} - {Course} {CourseGrade}";
+  }
+
+  public string AsCSV() {
+    return $"{ID},{FirstName},{LastName},{Course},{CourseGrade}\r\n";
+  }
+
+
+  public static bool TryParse(string value, out Student var) {
+    string[] values = value.Split(',', StringSplitOptions.RemoveEmptyEntries);
+    var = new Student();
+
+    if (values.Count() != 5)
+      return false;
+
+    if (!Int32.TryParse(values[0], out var.ID))
+      return false;
+    var.FirstName = values[1];
+    var.LastName = values[2];
+    var.Course = values[3];
+    if (!Single.TryParse(values[4], out var.CourseGrade))
+      return false;
+
+    var.UpdateAll();
+    return true;
+  }
+
+
+
+  public Student() {
+    ID = -1;
+    FirstName = "";
+    LastName = "";
+    Course = "";
+    CourseGrade = -1;
+    UpdateAll();
+  }
+  public Student(Student s) {
+    ID = s.ID;
+    FirstName = s.FirstName;
+    LastName = s.LastName;
+    Course = s.Course;
+    CourseGrade = s.CourseGrade;
+    UpdateAll();
+  }
+  public Student(int i, string fn, string ln, string c, int cg) {
+    ID = i;
+    FirstName = fn;
+    LastName = ln;
+    Course = c;
+    CourseGrade = cg;
+    UpdateAll();
+  }
+}
